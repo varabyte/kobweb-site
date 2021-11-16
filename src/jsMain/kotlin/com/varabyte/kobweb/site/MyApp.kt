0491eee7924e5527206e3642744ce79c22d119ea
David Herman <bitspittle@gmail.com>
2021-11-16 13:51:08 -0800

    Delete unused palette colors

--- src/jsMain/kotlin/com/varabyte/kobweb/site/MyApp.kt
+++ src/jsMain/kotlin/com/varabyte/kobweb/site/MyApp.kt
@@ -2,14 +2,12 @@ package com.varabyte.kobweb.site
 
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.ui.*
-import com.varabyte.kobweb.compose.ui.graphics.Color
 import com.varabyte.kobweb.compose.ui.graphics.toCssColor
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.*
 import com.varabyte.kobweb.silk.components.layout.Surface
-import com.varabyte.kobweb.silk.theme.SilkConfig
 import com.varabyte.kobweb.silk.theme.SilkTheme
-import com.varabyte.kobweb.silk.theme.colors.*
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import org.jetbrains.compose.web.css.*
 
 object CssGlobalsStyleSheet : StyleSheet() {
@@ -19,42 +17,6 @@ object CssGlobalsStyleSheet : StyleSheet() {
                 "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif")
         }
     }
-}
-
-val DarkSilkPalette = run {
-    val buttonBase = ColorSchemes.White._900
-    SilkPalette(
-        ColorMode.DARK,
-        background = Color.White,
-        color = Color(245, 245, 245, 1),
-        link = SilkPalette.Link(
-            default = Color.Blue,
-            visited = Color.Purple,
-        ),
-        button = SilkPalette.Button(
-            default = buttonBase,
-            hover = buttonBase.darkened(),
-            pressed = buttonBase.darkened().darkened()
-        )
-    )
-}
-
-val LightSilkPalette = run {
-    val buttonBase = ColorSchemes.Black._900
-    SilkPalette(
-        ColorMode.LIGHT,
-        background = Color.Black,
-        color = Color.Black,
-        link = SilkPalette.Link(
-            default = Color.Blue.lightened(),
-            visited = Color.Purple.lightened(),
-        ),
-        button = SilkPalette.Button(
-            default = buttonBase,
-            hover = buttonBase.lightened(),
-            pressed = buttonBase.lightened().lightened()
-        )
-    )
 }
 
 @InitSilk
