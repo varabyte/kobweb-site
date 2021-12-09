076230c52f6a97c0c924ff32f61e2cf39eabf4a9
David Herman <bitspittle@gmail.com>
2021-12-09 09:56:31 -0800

    Nit: Add note about highlight.js
    ... potentially useful since this is an open source project and people
    might want to see how we did it.

--- src/jsMain/kotlin/com/varabyte/kobweb/site/components/widgets/KotlinCode.kt
+++ src/jsMain/kotlin/com/varabyte/kobweb/site/components/widgets/KotlinCode.kt
@@ -13,6 +13,8 @@ import org.jetbrains.compose.web.dom.Text
 /**
  * Creates a code block that is colored by highlight.js
  */
+// Note: To enable this widget to work, we needed to add highlight.js support to this project. See the kobweb
+// block in our build.gradle.kts file to see how this was done.
 @Composable
 fun KotlinCode(code: String, modifier: Modifier = Modifier) {
     Pre(attrs = modifier.asAttributeBuilder()) {
