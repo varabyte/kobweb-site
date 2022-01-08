7b62013e6577cf96e73ea55c1c5584d068576b44
David Herman <bitspittle@gmail.com>
2022-01-07 23:47:34 -0800

    Update kobweb to 0.7.12
    ... and add references to the kobwebx-markdown artifact in
    preparation for using markdown someday.

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,7 @@ kotlin {
                 implementation(libs.kobweb.core)
                 implementation(libs.kobweb.silk.core)
                 implementation(libs.kobweb.silk.icons.fa)
+                implementation(libs.kobwebx.markdown)
              }
         }
 
