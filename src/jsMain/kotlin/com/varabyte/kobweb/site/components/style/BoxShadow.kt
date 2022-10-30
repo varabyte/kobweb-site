7f703e21d2e2b8c4133a9f04844c425746832733
David Herman <bitspittle@gmail.com>
2022-10-29 17:37:40 -0700

    Update Kobweb to v0.10.2

--- src/jsMain/kotlin/com/varabyte/kobweb/site/components/style/BoxShadow.kt
+++ src/jsMain/kotlin/com/varabyte/kobweb/site/components/style/BoxShadow.kt
@@ -1,14 +1,14 @@
 package com.varabyte.kobweb.site.components.style
 
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
 import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.styleModifier
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import org.jetbrains.compose.web.css.*
 
 fun Modifier.boxShadow(colorMode: ColorMode) = run {
-    val colorStr = when (colorMode) {
-        ColorMode.DARK -> "rgb(238, 238, 238, 0.2)"
-        ColorMode.LIGHT -> "rgb(17, 17, 17, 0.2)"
-    }
-    boxShadow("0 0 0 1px $colorStr")
+    boxShadow(spreadRadius = 1.px, color = when (colorMode) {
+        ColorMode.DARK -> Color.rgba(238, 238, 238, 0.2f)
+        ColorMode.LIGHT -> Color.rgba(17, 17, 17, 0.2f)
+    })
 }
