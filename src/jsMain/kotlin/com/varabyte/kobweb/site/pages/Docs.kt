f0ae7543f2807b1e02fdef24bf4783878d5d6f9f
David Herman <bitspittle@gmail.com>
2023-01-10 11:13:27 -0800

    Fix bad wrapping issue in Getting Started page.

--- src/jsMain/kotlin/com/varabyte/kobweb/site/pages/Docs.kt
+++ src/jsMain/kotlin/com/varabyte/kobweb/site/pages/Docs.kt
@@ -3,6 +3,7 @@ package com.varabyte.kobweb.site.pages
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.css.WhiteSpace
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.*
@@ -12,6 +13,7 @@ import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.site.components.layouts.PageLayout
 import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.*
 
 @Page
 @Composable
@@ -26,12 +28,11 @@ fun DocsPage() {
                 Modifier.fontSize(2.25.cssRem).fontWeight(FontWeight.Bolder)
             )
 
-            Row(Modifier.fontSize(1.cssRem).flexWrap(FlexWrap.Wrap).margin(top = 2.cssRem)) {
-                SpanText("Coming soon! Please refer to the ")
+            Span(Modifier.whiteSpace(WhiteSpace.PreWrap).margin(top = 2.cssRem).toAttrs()) {
+                Text("Coming soon! Please refer to the ")
                 Link("https://github.com/varabyte/kobweb/", "official project README")
-                SpanText(" until this page is populated.")
+                Text(" until this page is populated.")
             }
-
         }
     }
 }
