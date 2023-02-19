fc365f577560a3208eb3fef568389e194c07538c
David Herman <bitspittle@gmail.com>
2023-02-18 17:47:36 -0800

    Nit: Tweak header bar

--- src/jsMain/kotlin/com/varabyte/kobweb/site/components/sections/NavHeader.kt
+++ src/jsMain/kotlin/com/varabyte/kobweb/site/components/sections/NavHeader.kt
@@ -8,12 +8,27 @@ import com.varabyte.kobweb.compose.ui.*
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.navigation.Anchor
 import com.varabyte.kobweb.silk.components.icons.fa.*
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
+import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import com.varabyte.kobweb.silk.theme.colors.rememberColorMode
 import com.varabyte.kobweb.site.components.style.boxShadow
 import com.varabyte.kobweb.site.components.widgets.*
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.Img
+
+val NavHeaderStyle by ComponentStyle {
+    base {
+        Modifier
+            .zIndex(1)
+            .fillMaxWidth()
+            .backgroundColor(getNavBackgroundColor(colorMode))
+            .position(Position.Sticky)
+            .top(0.percent)
+            .backdropFilter(saturate(180.percent), blur(5.px))
+            .boxShadow(colorMode)
+    }
+}
 
 @Composable
 private fun HomeLogo() {
@@ -41,18 +56,7 @@ private val BUTTON_MARGIN = Modifier.margin(0.px, 10.px)
 @Composable
 fun NavHeader() {
     var colorMode by rememberColorMode()
-    Box(
-        Modifier
-            .zIndex(1)
-            .fillMaxWidth()
-            .backgroundColor(getNavBackgroundColor(colorMode))
-            .position(Position.Sticky)
-            .top(0.percent)
-            .backdropFilter(saturate(180.percent), blur(5.px))
-            .boxShadow(colorMode),
-
-        contentAlignment = Alignment.Center,
-    ) {
+    Box(NavHeaderStyle.toModifier(), contentAlignment = Alignment.Center) {
         Row(
             Modifier.fillMaxWidth(90.percent).margin(1.em),
             verticalAlignment = Alignment.CenterVertically
@@ -60,11 +64,11 @@ fun NavHeader() {
             HomeLogo()
             Spacer()
             Row(Modifier.margin(0.px, 12.px)) {
-                LinkButton("https://discord.gg/5NZ2GKV5Cs", BUTTON_MARGIN, shape = ButtonShape.CIRCLE) {
-                    FaDiscord()
-                }
                 LinkButton("https://github.com/varabyte/kobweb", BUTTON_MARGIN, shape = ButtonShape.CIRCLE) {
                     FaGithub()
+                }
+                LinkButton("https://discord.gg/5NZ2GKV5Cs", BUTTON_MARGIN, shape = ButtonShape.CIRCLE) {
+                    FaDiscord()
                 }
                 ThemedButton(
                     onClick = { colorMode = colorMode.opposite() },
