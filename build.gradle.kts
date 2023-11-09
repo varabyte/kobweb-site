2fa6a620cbe84ff7e66045bdcc994c727cf55e57
David Herman <bitspittle@gmail.com>
2023-11-09 10:34:36 -0800

    Update site to use a multimodule layout

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,8 @@
-import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
-import kotlinx.html.link
-import kotlinx.html.script
-
-plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.jetbrains.compose)
-    alias(libs.plugins.kobweb.application)
-    alias(libs.plugins.kobwebx.markdown)
-}
-
-repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
-    maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
-}
-
-group = "com.varabyte.kobweb.site"
-version = "1.0-SNAPSHOT"
-
-kobweb {
-    app {
-        index {
-            head.add {
-                link {
-                    rel = "stylesheet"
-                    href = "/highlight.js/styles/dracula.css"
-                }
-                script {
-                    src = "/highlight.js/highlight.min.js"
-                }
-            }
-        }
-    }
-}
-
-kotlin {
-    configAsKobwebApplication("kobweb-site")
-
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(compose.runtime)
-            }
-        }
-
-        val jsMain by getting {
-            dependencies {
-                implementation(compose.html.core)
-                implementation(libs.kobweb.core)
-                implementation(libs.kobweb.silk)
-                implementation(libs.silk.icons.fa)
-                implementation(libs.kobwebx.markdown)
-             }
-        }
+subprojects {
+    repositories {
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,3 +7,5 @@ pluginManagement {
 }
 
 rootProject.name = "kobweb-site"
+
+include(":site")
