13825592d9d71ee302600cb933b8f9c317ddbff9
David Herman <bitspittle@gmail.com>
2024-08-13 15:35:17 -0700

    Fix deprecation

--- site/src/jsMain/kotlin/com/varabyte/kobweb/site/components/widgets/code/CodeBlock.kt
+++ site/src/jsMain/kotlin/com/varabyte/kobweb/site/components/widgets/code/CodeBlock.kt
@@ -5,9 +5,9 @@ import com.varabyte.kobweb.compose.css.Overflow
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
-import com.varabyte.kobweb.silk.components.style.ComponentStyle
-import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
-import com.varabyte.kobweb.silk.components.style.toModifier
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.style.toModifier
 import com.varabyte.kobweb.site.components.style.SiteTextSize
 import com.varabyte.kobweb.site.components.style.siteText
 import org.jetbrains.compose.web.css.percent
@@ -17,7 +17,7 @@ import org.jetbrains.compose.web.dom.Code
 import org.jetbrains.compose.web.dom.Pre
 import org.jetbrains.compose.web.dom.Text
 
-val CodeBlockStyle by ComponentStyle {
+val CodeBlockStyle = CssStyle {
     // For some reason I'm not smart enough to figure out, code blocks are messing up the layout on mobile - they lay
     // themselves out too wide and break out of the central column. Here, we just constrain them to whatever the
     // view width is, until we are on desktop and the column is no longer so small as to cause an issue.
