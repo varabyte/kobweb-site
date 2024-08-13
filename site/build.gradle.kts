254055b0eda77b9e43b6b5c05d0da4f69f30b114
David Herman <bitspittle@gmail.com>
2024-08-13 16:45:40 -0700

    Migrate site-listing build logic into markdown.process

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -52,9 +52,9 @@ kobweb {
             }
         }
 
-//        process.set { entries ->
-//            generateDocs(entries)
-//        }
+        process.set { entries ->
+            SiteListingGenerator.generate(this, entries)
+        }
     }
 }
 
@@ -80,184 +80,106 @@ kotlin {
     }
 }
 
-class MarkdownVisitor : AbstractVisitor() {
-    private val _frontMatter = mutableMapOf<String, List<String>>()
-    val frontMatter: Map<String, List<String>> = _frontMatter
-
-    override fun visit(customBlock: CustomBlock) {
-        if (customBlock is YamlFrontMatterBlock) {
-            val yamlVisitor = YamlFrontMatterVisitor()
-            customBlock.accept(yamlVisitor)
-            _frontMatter.putAll(yamlVisitor.data)
-        }
+object SiteListingGenerator {
+    class Article(val slug: String)
+    class Subcategory(
+        val title: String,
+        vararg val articles: Article
+    ) {
+        constructor(vararg articles: Article) : this("", *articles)
     }
-}
 
-class Article(val slug: String)
-class Subcategory(
-    val title: String,
-    vararg val articles: Article
-) {
-    constructor(vararg articles: Article) : this("", *articles)
-}
+    class Category(
+        val slug: String,
+        val title: String,
+        vararg val subcategories: Subcategory
+    ) {
+        constructor(slug: String, vararg subcategories: Subcategory) :
+                this(slug, slug.capitalized(), *subcategories)
+    }
 
-class Category(
-    val slug: String,
-    val title: String,
-    vararg val subcategories: Subcategory
-) {
-    constructor(slug: String, vararg subcategories: Subcategory) :
-            this(slug, slug.capitalized(), *subcategories)
-}
+    class ArticleMetadata(val title: String)
+    class ArticleEntry(val slug: String, val metadata: ArticleMetadata)
 
-private val SITE_LISTING = buildList {
-    add(
-        Category(
-            "guides",
-            Subcategory(
-                "First Steps",
-                Article("gettingstarted"),
-                Article("gettingkobweb"),
-            ),
+    fun List<Category>.find(categorySlug: String, articleSlug: String): Article? {
+        return this.asSequence()
+            .filter { it.slug == categorySlug }
+            .flatMap { it.subcategories.asSequence() }
+            .flatMap { it.articles.asSequence() }
+            .firstOrNull { it.slug == articleSlug }
+    }
+
+    private val SITE_LISTING = buildList {
+        add(
+            Category(
+                "guides",
+                Subcategory(
+                    "First Steps",
+                    Article("getting-started"),
+                    Article("getting-kobweb"),
+                    Article("installing-kobweb"),
+                ),
+            )
         )
-    )
 
-    add(
-        Category(
-            "widgets",
-            Subcategory(
-                "Forms",
-                Article("button"),
-            ),
-            Subcategory(
-                "Overlay",
-                Article("tooltip"),
-            ),
+        add(
+            Category(
+                "widgets",
+                Subcategory(
+                    "Forms",
+                    Article("button"),
+                ),
+                Subcategory(
+                    "Overlay",
+                    Article("tooltip"),
+                ),
+            )
         )
-    )
 
-    add(
-        Category(
-            "tutorials",
-            Subcategory(
-                Article("createfirstsite")
-            ),
+        add(
+            Category(
+                "tutorials",
+                Subcategory(
+                    Article("create-first-site")
+                ),
+            )
         )
-    )
-}
+    }
 
-class ArticleMetadata(val title: String)
-class ArticleEntry(val slug: String, val metadata: ArticleMetadata)
+    fun generate(scope: MarkdownBlock.ProcessScope, entries: List<MarkdownEntry>) {
+        scope._generate(entries.filter { it.route.startsWith("/docs/") })
+    }
 
-fun List<Category>.find(categorySlug: String, articleSlug: String): Article? {
-    return this.asSequence()
-        .filter { it.slug == categorySlug }
-        .flatMap { it.subcategories.asSequence() }
-        .flatMap { it.articles.asSequence() }
-        .firstOrNull { it.slug == articleSlug }
-}
+    @Suppress("FunctionName") // Underscore avoids ambiguity error
+    private fun MarkdownBlock.ProcessScope._generate(entries: List<MarkdownEntry>) {
+        val requiredFields = listOf("title")
+        val discoveredMetadata = mutableMapOf<String, MutableList<ArticleEntry>>()
 
-fun MarkdownBlock.ProcessScope.generateDocs(entries: List<MarkdownEntry>) {
-    val entries = entries.filter { it.route.startsWith("/docs/") }
-    println("HELLO WITH ${entries.size} ENTRIES")
-}
-
-val generateSiteListingTask = task("generateSiteListing") {
-    group = "kobweb site"
-    val LISTINGS_INPUT_DIR = "src/jsMain/resources/markdown/docs"
-    val LISTINGS_OUTPUT_FILE = "src/jsMain/kotlin/com/varabyte/kobweb/site/model/listing/SiteListing.kt"
-
-    val discoveredMetadata = mutableMapOf<String, MutableList<ArticleEntry>>()
-
-    inputs.dir(LISTINGS_INPUT_DIR)
-    outputs.file(layout.projectDirectory.file(LISTINGS_OUTPUT_FILE))
-
-    doLast {
-        val parser = kobweb.markdown.features.createParser()
-        val root = file(LISTINGS_INPUT_DIR)
-        fileTree(root).forEach { mdArticle ->
-            val rootNode = parser.parse(mdArticle.readText())
-            val visitor = MarkdownVisitor()
-
-            rootNode.accept(visitor)
-
-            val fm = visitor.frontMatter
-            val requiredFields = listOf("title")
+        entries.forEach { entry ->
             val (title) = requiredFields
-                .map { key -> fm[key]?.singleOrNull() }
+                .map { key -> entry.frontMatter[key]?.singleOrNull() }
                 .takeIf { values -> values.all { it != null } }
                 ?.requireNoNulls()
                 ?: run {
-                    println("Skipping $mdArticle in the listing as it is missing required frontmatter fields (one of $requiredFields)")
+                    println("Skipping ${entry.route} in the listing as it is missing required frontmatter fields (one of $requiredFields)")
                     return@forEach
                 }
 
-            val categorySlug = mdArticle.parentFile.name
+            val articleFile = File(entry.filePath)
+            val categorySlug = articleFile.parentFile.name
             discoveredMetadata.getOrPut(categorySlug) { mutableListOf() }
                 .add(
                     ArticleEntry(
-                        mdArticle.nameWithoutExtension.lowercase(),
+                        entry.route.substringAfterLast('/'),
                         ArticleMetadata(title)
-                    ).also { entry ->
-                        if (SITE_LISTING.find(categorySlug, entry.slug) == null) {
+                    ).also { article ->
+                        if (SITE_LISTING.find(categorySlug, article.slug) == null) {
                             throw GradleException(
-                                "$mdArticle needs an entry (slug: \"${entry.slug}\") in `SITE_LISTING`."
+                                "${entry.route} needs an entry (slug: \"${article.slug}\") in `SITE_LISTING`."
                             )
                         }
                     }
                 )
-        }
-
-        project.layout.projectDirectory.file(LISTINGS_OUTPUT_FILE).asFile.let { siteListing ->
-            val indent = "   "
-
-            siteListing.parentFile.mkdirs()
-            siteListing.writeText(buildString {
-                appendLine(
-                    """
-                    package com.varabyte.kobweb.site.model.listing
-
-                    // DO NOT EDIT THIS FILE BY HAND! IT GETS AUTO-GENERATED BY `./gradlew $name`
-                    // Instead, edit the SITE_LISTING constant in `build.gradle.kts` and re-run the task.
-
-                    val SITE_LISTING = buildList {
-                    """.trimIndent()
-                )
-
-                SITE_LISTING.forEach { category ->
-                    appendLine("${indent}add(")
-                    appendLine("${indent}${indent}Category(")
-                    appendLine("${indent}${indent}${indent}\"${category.slug}\",")
-                    appendLine("${indent}${indent}${indent}\"${category.title}\",")
-                    category.subcategories.forEach { subcategory ->
-                        appendLine("${indent}${indent}${indent}Subcategory(")
-                        appendLine("${indent}${indent}${indent}${indent}\"${subcategory.title}\",")
-                        subcategory.articles.forEach { article ->
-                            val metadata = discoveredMetadata.getValue(category.slug).first { it.slug == article.slug }.metadata
-                            appendLine("${indent}${indent}${indent}${indent}Article(\"${article.slug}\", \"${metadata.title}\"),")
-                        }
-                        appendLine("${indent}${indent}${indent}),")
-                    }
-                    appendLine("${indent}${indent})")
-                    appendLine("${indent})")
-                }
-
-                appendLine(
-                    """
-                    }
-
-                    fun List<Category>.findArticle(categorySlug: String, articleSlug: String): Article? {
-                       return this.asSequence()
-                          .filter { it.slug == categorySlug }
-                          .flatMap { it.subcategories.asSequence() }
-                          .flatMap { it.articles.asSequence() }
-                          .firstOrNull { it.slug == articleSlug }
-                    }
-                    """.trimIndent()
-                )
-            })
-
-            println("Generated ${siteListing.absolutePath}")
         }
 
         SITE_LISTING.forEach { category ->
@@ -271,5 +193,52 @@ val generateSiteListingTask = task("generateSiteListing") {
                 }
             }
         }
+
+        generateKotlin("com/varabyte/kobweb/site/model/listing/SiteListing.kt", buildString {
+            val indent = "   "
+            appendLine(
+                """
+                    package com.varabyte.kobweb.site.model.listing
+
+                    // DO NOT EDIT THIS FILE BY HAND! IT IS GETTING AUTO-GENERATED BY GRADLE
+                    // Instead, edit the SITE_LISTING constant in `build.gradle.kts` and re-run the task.
+
+                    val SITE_LISTING = buildList {
+                    """.trimIndent()
+            )
+
+            SITE_LISTING.forEach { category ->
+                appendLine("${indent}add(")
+                appendLine("${indent}${indent}Category(")
+                appendLine("${indent}${indent}${indent}\"${category.slug}\",")
+                appendLine("${indent}${indent}${indent}\"${category.title}\",")
+                category.subcategories.forEach { subcategory ->
+                    appendLine("${indent}${indent}${indent}Subcategory(")
+                    appendLine("${indent}${indent}${indent}${indent}\"${subcategory.title}\",")
+                    subcategory.articles.forEach { article ->
+                        val metadata =
+                            discoveredMetadata.getValue(category.slug).first { it.slug == article.slug }.metadata
+                        appendLine("${indent}${indent}${indent}${indent}Article(\"${article.slug}\", \"${metadata.title}\"),")
+                    }
+                    appendLine("${indent}${indent}${indent}),")
+                }
+                appendLine("${indent}${indent})")
+                appendLine("${indent})")
+            }
+
+            appendLine(
+                """
+                    }
+
+                    fun List<Category>.findArticle(categorySlug: String, articleSlug: String): Article? {
+                       return this.asSequence()
+                          .filter { it.slug == categorySlug }
+                          .flatMap { it.subcategories.asSequence() }
+                          .flatMap { it.articles.asSequence() }
+                          .firstOrNull { it.slug == articleSlug }
+                    }
+                    """.trimIndent()
+            )
+        })
     }
 }
