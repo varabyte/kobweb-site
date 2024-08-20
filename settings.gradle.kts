d58cd3f501507fd87dce0af018d91e55e2507ad4
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-08-20 11:49:17 -0700

    Nit: Remove unecessary `mavenLocal` declaration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 pluginManagement {
     repositories {
-        mavenLocal()
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
