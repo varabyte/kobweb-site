484a917612b17eea96a22b9c3280cf5d6af0d126
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-10-06 20:53:55 -0400

    Redesign docs layout/styling

--- site/src/jsMain/kotlin/com/varabyte/kobweb/site/AppEntry.kt
+++ site/src/jsMain/kotlin/com/varabyte/kobweb/site/AppEntry.kt
@@ -2,8 +2,10 @@ package com.varabyte.kobweb.site
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import com.varabyte.kobweb.compose.css.BorderCollapse
 import com.varabyte.kobweb.compose.css.FontWeight
-import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.css.ListStyleType
+import com.varabyte.kobweb.compose.css.ScrollBehavior
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Color
 import com.varabyte.kobweb.compose.ui.modifiers.*
@@ -12,14 +14,15 @@ import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
+import com.varabyte.kobweb.silk.init.layer
 import com.varabyte.kobweb.silk.init.registerStyleBase
 import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
+import com.varabyte.kobweb.silk.style.layer.SilkLayer
 import com.varabyte.kobweb.silk.style.toModifier
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import com.varabyte.kobweb.site.components.style.DividerColor
 import kotlinx.browser.localStorage
-import org.jetbrains.compose.web.css.cssRem
-import org.jetbrains.compose.web.css.vh
+import org.jetbrains.compose.web.css.*
 
 private const val COLOR_MODE_KEY = "app:colorMode"
 
@@ -28,14 +31,51 @@ fun initSilk(ctx: InitSilkContext) {
     ctx.config.initialColorMode = localStorage.getItem(COLOR_MODE_KEY)?.let { ColorMode.valueOf(it) } ?: ColorMode.DARK
 
     ctx.stylesheet.apply {
-        registerStyleBase("body") { Modifier.fontFamily("Roboto", "sans-serif") }
-        registerStyleBase("code, pre") { Modifier.fontFamily("Roboto Mono", "monospace") }
+//        registerStyleBase("body") { Modifier.fontFamily("Roboto", "sans-serif") }
+//        registerStyleBase("code, pre") { Modifier.fontFamily("Roboto Mono", "monospace") }
+        registerStyleBase("body") {
+            Modifier.fontFamily(
+                "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
+                "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"
+            )
+        }
 
-        val headerCommon = Modifier.fontWeight(FontWeight.Bold).textAlign(TextAlign.Center).margin(top = 2.cssRem, bottom = 1.cssRem)
-        registerStyleBase("h1") { headerCommon.fontSize(3.5.cssRem) }
-        registerStyleBase("h2") { headerCommon.fontSize(2.5.cssRem) }
-        registerStyleBase("h3") { headerCommon.fontSize(1.5.cssRem) }
-        registerStyleBase("h4") { headerCommon.fontSize(1.35.cssRem) }
+        registerStyle("html") {
+            base {
+                Modifier.scrollPadding(top = 5.5.cssRem)
+            }
+            cssRule(CSSMediaQuery.MediaFeature("prefers-reduced-motion", StylePropertyValue("no-preference"))) {
+                Modifier.scrollBehavior(ScrollBehavior.Smooth)
+            }
+        }
+        layer(SilkLayer.BASE) {
+            registerStyleBase("ul, ol, menu") {
+                Modifier
+                    .listStyle(ListStyleType.None)
+                    .padding(0.px)
+                    .margin(0.px)
+            }
+            registerStyleBase("table") {
+                Modifier.borderCollapse(BorderCollapse.Collapse)
+            }
+        }
+
+        val headerCommon = Modifier.fontWeight(FontWeight.Bold).margin(top = 1.25.cssRem, bottom = 0.75.cssRem)
+        registerStyleBase("h1") {
+            headerCommon.fontSize(3.cssRem)
+        }
+
+        registerStyleBase("h2") {
+            headerCommon.fontSize(2.cssRem)
+        }
+
+        registerStyleBase("h3") {
+            headerCommon.fontSize(1.5.cssRem)
+        }
+
+        registerStyleBase("h4") {
+            headerCommon
+        }
     }
 }
 
--- site/src/jsMain/kotlin/com/varabyte/kobweb/site/components/layouts/DocsLayout.kt
+++ site/src/jsMain/kotlin/com/varabyte/kobweb/site/components/layouts/DocsLayout.kt
@@ -1,28 +1,95 @@
 package com.varabyte.kobweb.site.components.layouts
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.css.*
+import com.varabyte.kobweb.compose.css.Transition
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Row
-import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.PageContext
 import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.navigation.LinkVars
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.style.breakpoint.displayIfAtLeast
+import com.varabyte.kobweb.silk.style.common.SmoothColorTransitionDurationVar
+import com.varabyte.kobweb.silk.style.selectors.descendants
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.style.vars.color.BorderColorVar
+import com.varabyte.kobweb.silk.theme.colors.palette.background
+import com.varabyte.kobweb.silk.theme.colors.palette.toPalette
+import com.varabyte.kobweb.silk.theme.colors.shifted
+import com.varabyte.kobweb.site.components.sections.listing.ListingSidebar
 import com.varabyte.kobweb.site.components.sections.listing.MobileLocalNav
-import com.varabyte.kobweb.site.components.style.SiteTextSize
-import com.varabyte.kobweb.site.components.style.siteText
 import com.varabyte.kobweb.site.model.listing.ArticleHandle
 import com.varabyte.kobweb.site.model.listing.SITE_LISTING
 import com.varabyte.kobweb.site.model.listing.findArticle
 import com.varabyte.kobwebx.markdown.markdown
-import org.jetbrains.compose.web.css.cssRem
-import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Article
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Main
 
 fun PageContext.RouteInfo.toArticleHandle(): ArticleHandle? {
     return SITE_LISTING.findArticle(path)
+}
+
+val ArticleStyle = CssStyle {
+    descendants("ul", "ol", "menu") {
+        Modifier
+            .listStyle(ListStyleType.Revert)
+            .paddingInline(2.cssRem)
+    }
+    descendants("table") {
+        Modifier
+            .display(DisplayStyle.Block)
+            .width(Width.MaxContent)
+            .maxWidth(100.percent)
+            .overflow(Overflow.Auto)
+            .margin(bottom = 1.cssRem)
+    }
+    descendants("table td", "table th") {
+        Modifier
+            .border(1.px, LineStyle.Solid, BorderColorVar.value())
+            .padding(6.px, 13.px)
+    }
+    descendants("table tr:nth-child(2n)") {
+        Modifier
+            .backgroundColor(colorMode.toPalette().background.shifted(colorMode, 0.05f))
+    }
+    descendants("code") {
+        Modifier
+            .whiteSpace(WhiteSpace.BreakSpaces)
+            .overflowWrap(OverflowWrap.BreakWord)
+            .padding(2.px, 4.px)
+            .borderRadius(4.px)
+            .backgroundColor(
+                colorMode.toPalette().background.shifted(colorMode, if (colorMode.isLight) 0.1f else 0.18f)
+            )
+            .transition(Transition.of("background-color", SmoothColorTransitionDurationVar.value()))
+    }
+    descendants("pre") {
+        Modifier
+            .borderRadius(0.5.cssRem)
+    }
+    descendants("pre > code") {
+        Modifier
+            // Alternatively, don't set white-space for "code" above. OR modify the above selector to only apply to
+            // "code" blocks not inside "pre" block
+            .whiteSpace(WhiteSpace.Inherit)
+    }
+
+    // Use the default color for visited links, otherwise internal links look out of place
+    // Many docs site, but not all (e.g. MDN), also do this
+    descendants("a:any-link") {
+        Modifier.color(LinkVars.DefaultColor.value())
+    }
+    // This is an alternative to scrollPadding on the html
+//    descendants("h2", "h3", "h4", "h5", "h6") {
+//        Modifier.scrollMargin(top = 5.5.cssRem)
+//    }
 }
 
 @Composable
@@ -36,15 +103,48 @@ fun DocsLayout(content: @Composable () -> Unit) {
 
     PageLayout(title) {
         MobileLocalNav({})
-        Row(Modifier.fillMaxWidth().maxWidth(800.px).gap(1.cssRem).fillMaxHeight().siteText(SiteTextSize.NORMAL)) {
-//            ListingSideBar()
-            Column(Modifier.fillMaxSize()) {
-                if (articleHandle != null) {
-                    H1(Modifier.align(Alignment.CenterHorizontally).toAttrs()) {
-                        Text(articleHandle.article.title)
-                    }
+        Row(
+            Modifier
+                .margin(leftRight = autoLength) // Centers content
+                .fillMaxSize()
+                .maxWidth(80.cssRem),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Div(
+                Modifier
+                    .position(Position.Sticky)
+                    .top(5.cssRem)
+                    .toAttrs()
+            ) {
+                ListingSidebar(
+                    SITE_LISTING,
+                    Modifier
+                        .padding(top = 2.cssRem, left = 2.cssRem)
+                        .width(15.cssRem)
+                        .fillMaxHeight()
+                        .displayIfAtLeast(Breakpoint.MD)
+                )
+            }
+            Main(
+                ArticleStyle.toModifier()
+                    .minWidth(0.px)
+                    .padding(2.cssRem)
+                    .fillMaxWidth()
+                    .toAttrs()
+            ) {
+                Article {
+                    content()
                 }
-                content()
+            }
+            Div(
+                Modifier
+                    .displayIfAtLeast(Breakpoint.LG)
+                    .position(Position.Sticky)
+                    .top(5.cssRem)
+                    .toAttrs()
+            ) {
+                Div(Modifier.displayIfAtLeast(Breakpoint.MD).width(16.cssRem).toAttrs())
+                //Toc()
             }
         }
     }
