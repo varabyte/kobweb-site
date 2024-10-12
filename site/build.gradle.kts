d5046a10e2c70d21a47a9db4afee468270d2869f
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-10-12 17:25:41 -0400

    Nit: Fix warning output

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -133,7 +133,7 @@ object SiteListingGenerator {
         }
 
         if (initialArticles.size != 1) {
-            println("There should only be one starting article, but one of these articles are missing a `follows` frontmatter value: $initialArticles")
+            println("There should only be one starting article, but one of these articles are missing a `follows` frontmatter value: ${initialArticles.map { it.toPath() }}")
         }
 
         val orderedArticleList = mutableListOf<MarkdownEntry>()
