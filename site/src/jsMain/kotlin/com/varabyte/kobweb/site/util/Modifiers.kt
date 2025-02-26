3386a0be52e361d6560f212aa7a9d880205e6ee0
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2025-02-26 13:00:21 -0500

    Delete unused style

--- site/src/jsMain/kotlin/com/varabyte/kobweb/site/util/Modifiers.kt
+++
@@ -1,6 +0,0 @@
-package com.varabyte.kobweb.site.util
-
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.tabIndex
-
-fun Modifier.focusable() = tabIndex(0)
