c3b3f04811b76dbb2b52db41c97f554a5dff8a2d
David Herman <bitspittle@gmail.com>
2025-02-26 18:03:23 -0800

    Nit: Set site description

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -21,6 +21,8 @@ version = "1.0-SNAPSHOT"
 kobweb {
     app {
         index {
+            description.set("The official Kobweb site")
+
             head.add {
                 link {
                     rel = "stylesheet"
